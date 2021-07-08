1f490cbf5ea942dad98ca4dfdfb4c0d20a2a0e41
Alphakas <alphakas@Alphakass-iMac.local>
2021-07-09 01:52:41 +0530

    adstest

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b7a18420ee840121629"; }
+/******/ 		__webpack_require__.h = function() { return "00c0b2116a4b750cee57"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "../styles/global.css";
+import Head from 'next/head';
 
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps}/>
+  return <> 
+  <Head>
+    <title>Precious Things</title>
+      <meta name="exoclick-site-verification" content="63cb7b2c7c614d322f8e0f7d9b3fbbc5"/>
+    </Head> 
+  <Component {...pageProps}/>
+  <script type="application/javascript">
+    var ad_idzone = "4345132",
+    ad_width = "728",
+    ad_height = "90",
+    v_pos = "top",
+    h_pos = "left";
+</script>
+<script type="application/javascript" src="https://a.realsrv.com/js.php?t=17&idzone=4345132"></script>
+  </>
 }
