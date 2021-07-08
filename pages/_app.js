33b9e789d0f5b4fd470080e1d303f4cd37ee859d
Alphakas <alphakas@Alphakass-iMac.local>
2021-07-09 01:55:11 +0530

    changed to body

--- pages/_app.js
+++ pages/_app.js
@@ -8,13 +8,5 @@ export default function App({Component, pageProps}) {
       <meta name="exoclick-site-verification" content="63cb7b2c7c614d322f8e0f7d9b3fbbc5"/>
     </Head> 
   <Component {...pageProps}/>
-  <script type="application/javascript">
-    var ad_idzone = "4345132",
-    ad_width = "728",
-    ad_height = "90",
-    v_pos = "top",
-    h_pos = "left";
-</script>
-<script type="application/javascript" src="https://a.realsrv.com/js.php?t=17&idzone=4345132"></script>
   </>
 }
---
+++ pages/_documents.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script type="application/javascript">
+    var ad_idzone = "4345132",
+    ad_width = "728",
+    ad_height = "90",
+    v_pos = "top",
+    h_pos = "left";
+</script>
+<script type="application/javascript" src="https://a.realsrv.com/js.php?t=17&idzone=4345132"></script>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
